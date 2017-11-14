48dfd8f23250576bbf9b42866a2e347880cc0442
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-11-14 05:00:50 +0100

    chore(build): Fix release script

--- release.sh
+++ release.sh
@@ -2,9 +2,11 @@
 git checkout develop
 mvn jgitflow:release-start && \
  mvn jgitflow:release-finish -DnoDeploy=true && \
- git checkout master && \
  conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && \
  git add CHANGELOG.md && \
  git commit -m "Changelog" && \
+ git checkout master && \
+ git merge develop && \
  git push origin master && git push --tags && \
  git checkout develop
+
