f273fb9cccd55a0507a473d7480d25ce581c688c
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-12-11 08:14:13 +0100

    chore(build): Make build great again

--- release.sh
+++ release.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 git checkout develop
 mvn jgitflow:release-start && \
- mvn jgitflow:release-finish -DnoDeploy=true && \
  conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && \
  git add CHANGELOG.md && \
  git commit -m "Changelog" && \
- git checkout master && \
- git merge develop && \
+ mvn jgitflow:release-finish -DnoDeploy=true && \
  git push origin master && git push --tags && \
  git checkout develop
-
