4b32366c93cc3c67048821af88d1cc7bd76ee160
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-12-30 10:36:25 +0100

    tests(call-builder): Try to fix unit test errors

--- src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderTest.java
+++ src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderTest.java
@@ -9,6 +9,8 @@ public class RocketChatClientCallBuilderTest {
   @Test(expected = IOException.class)
   public void shouldEscapeSpecialCharacters() throws Exception {
     // given
+    System.clearProperty("http.proxyHost");
+    System.clearProperty("http.proxyPort");
     RocketChatClientCallBuilder rocketCallBuilder = new RocketChatClientCallBuilder("https://open.rocket.chat/", false, "]\",", "]\",");
     // when
     rocketCallBuilder.buildCall(RocketChatRestApiV1.ChannelsList);
